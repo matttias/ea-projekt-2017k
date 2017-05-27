667642e290513940997c9bf08f67b6fcf33302c5
Karl-Erik Borkmann <karlerikb@users.noreply.github.com>
2017-05-27 10:16:21 +0300

    edasiarendatud maatriksite genereerimine ja kuvamine

--- projekt.js
+++ projekt.js
@@ -19,131 +19,192 @@ window.onclick = function(event) {
 }
 */
 //Välja printimiseks
-var x1, x2, y1, y2;
-var matrix = [];
-var matrix1 = [];
-var matrix2 = [];
-var answerMatrix1 = [];
-var answerMatrix2 = [];
-var inputBoxvalue1;
-var inputBoxvalue2;
-var IDCounter = 0;
+// var x1, x2, y1, y2;
+// var matrix = [];
+// var matrix1 = [];
+// var matrix2 = [];
+// var answerMatrix1 = [];
+// var answerMatrix2 = [];
+// var inputBoxvalue1;
+// var inputBoxvalue2;
+// var IDCounter = 0;
+
+
+// **** GLOBAALSED MUUTUJAD ****
+
+var m1x, m1y, m2x, m2y;
 
 
 
-function ConfrmSelect() {
-    x1 = document.getElementById("XSelect1").value;
-	y1 = document.getElementById("YSelect1").value;
+// **** ÜLDINE FUNKTSIOON MAATRIKSITE GENEREERIMISEKS ****
+
+function generateMatrix() {
 	
-	x2 = document.getElementById("XSelect2").value;
-	y2 = document.getElementById("YSelect2").value;
+	m1x = document.getElementById("m1x").value;
+	m1y = document.getElementById("m1y").value;
+	m2x = document.getElementById("m2x").value;
+	m2y = document.getElementById("m2y").value;
 	
-	if(y1 == x2){
-		//http://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
+	
+	var m1 = document.getElementById("matrix1");
+	var m2 = document.getElementById("matrix2");
+	var mA = document.getElementById("matrixAnswer");
+	
+	if(m1y === m2x) {
+		
 		console.log("Saab arvutada");
-		//ei ole kindel, kas teeb maatriksid õigesti.
-		/*
-		for(var i=0; i<x1; i++) {
-			matrix[i] = [];
-			for(var j=0; j<y1; j++) {
-				matrix[i][j] = undefined;
-				console.log(matrix[i][j]);
-				//CreateInput(x1);
-			}
-		}
+		console.log("Esimene maatriks on m1x x m1y (" + m1x + " x " + m1y + ")");
+		console.log("Teine maatriks on m2x x m2y (" + m2x + " x " + m2y + ")");
 		
-		for(var i=0; i<x2; i++) {
-			matrix[i] = [];
-			for(var j=0; j<y2; j++) {
-				matrix[i][j] = undefined;
-				console.log(matrix[i][j]);
-				//CreateInput();
-			}
+		if(m1 && m2 && mA) {
+			
+			m1.innerHTML = "";
+			m2.innerHTML = "";
+			mA.innerHTML = "";
+			
+			createMatrix1();
+			createMatrix2();
+			createMatrixAnswer();
+			
+		} else {
+			
+			createMatrix1();
+			createMatrix2();
+			createMatrixAnswer();
 		}
-		*/
-		
-		CreateTableInput1();
-		CreateTableInput2();
 		
 	} else {
+		
 		console.log("Ei saa arvutada");
-		alert("Arvutada ei saa. Muuda maaatriksi suurusi.");
+		alert("Ei saa genereerida, muuda maatriksite suuruseid!");
+		
 	}
-	
-}
-
-function CreateTableInput1(){
-	/*var table = "<table>";
-	for (var i=0; i<x1; i++) {
-		table = table + "<tr><td>" + "<input>" + "</td><td>" + "<input>" + "</td></tr>"; 
-	}
-	table = table + "</table>";
-	document.getElementById('InputTable1').innerHTML = table;
-
-	
-	table = "<table>";
-	//table = table + "<tr><th>" + "Mänigja nimi" + "</th><th>" + "Tulemus" + "</th><tr>";
-	for (var i=0; i<x2; i++) {
-		table = table + "<tr><td>" + "<input>" + "</td><td>" + "<input>" + "</td></tr>"; 
-	}
-	table = table + "</table>";
-	document.getElementById('InputTable2').innerHTML = table;	
-	*/
-	
-	var InputTable1 = document.getElementById("InputTable1");
-	// creates a <table> element and a <tbody> element
-	var tbl = document.createElement("table");
-	var tblBody = document.createElement("tbody");
-	// creating all cells
-	for(var i=0; i<x1; i++) {
-	// creates a table row
-		var row = document.createElement("tr");
-		for (var j=0; j<x2; j++) {
-			IDCounter++;
-			//console.log(IDCounter);
-			//var x = document.createElement("INPUT");
-			//x.setAttribute("id", "inputmatrix");
-			//x.setAttribute("type", "text");
-			//document.body.appendChild(x);
-			//var cell = document.createElement("td");
-			var cell = document.createElement("INPUT");
-			cell.setAttribute("id", "inputmatrix" + IDCounter);
-			cell.setAttribute("type", "text");
-			//var cellText = document.createTextNode("cell in row "+i+", column "+j);
-			//cell.appendChild(cellText);
-			row.appendChild(cell);
-		}
-		// add the row to the end of the table body
-		tblBody.appendChild(row);
-	}
-	// put the <tbody> in the <table>
-	tbl.appendChild(tblBody);
-	// appends <table> into InputTable1
-	InputTable1.appendChild(tbl);
-	// sets the border attribute of tbl to 2;
-	//tbl.setAttribute("border", "2");
 }
 
 
-function CreateTableInput2(){
-	IDCounter = 30;
-	//var InputTable2 = document.getElementById("InputTable2");
-	var tbl = document.createElement("table");
-	var tblBody = document.createElement("tbody");
-	for(var i=0; i<x1; i++) {
+
+// **** FUNKTSIOON, MIS GENEREERIB ESIMESE MAATRIKSI ****
+
+function createMatrix1() {
+	
+	var matrix1Container = document.getElementById("matrix1Container");
+	m1Width = 42 * m1y;
+	m1Height = 28 * m1x;
+	matrix1Container.style.width = m1Width + "px";
+	matrix1Container.style.height = m1Height + "px";
+	
+	var matrix1 = document.getElementById("matrix1");
+	var tableBody = document.createElement("tbody");
+	
+	for(var i = 0; i < m1x; i++) {
 		var row = document.createElement("tr");
-		for(var j=0; j<x2; j++) {
-			IDCounter++;
-			var cell = document.createElement("INPUT");
-			cell.setAttribute("id", "inputmatrix" + IDCounter);
+		
+		for(var j = 0; j < m1y; j++) {
+			var rowId = i + 1;
+			var colId = j + 1;
+			var cell = document.createElement("input");
+			cell.setAttribute("id", "a" + rowId + colId);
 			cell.setAttribute("type", "text");
 			row.appendChild(cell);
+			
 		}
-		tblBody.appendChild(row);
+		tableBody.appendChild(row);
 	}
-	tbl.appendChild(tblBody);
-	InputTable2.appendChild(tbl);
+	matrix1.appendChild(tableBody);
 }
+
+
+
+// **** FUNKTSIOON, MIS GENEREERIB TEISE MAATRIKSI ****
+
+function createMatrix2() {
+	
+	var matrix2Container = document.getElementById("matrix2Container");
+	m2Width = 42 * m2y;
+	m2Height = 28 * m2x;
+	
+	m1Width = 42 * m1y;
+	m2Position = m1Width + 20;
+	
+	matrix2Container.style.width = m2Width + "px";
+	matrix2Container.style.height = m2Height + "px";
+	matrix2Container.style.left = m2Position + "px";
+	
+	var matrix2 = document.getElementById("matrix2");
+	var tableBody = document.createElement("tbody");
+	
+	for(var i = 0; i < m2x; i++) {
+		var row = document.createElement("tr");
+		
+		for(var j = 0; j < m2y; j++) {
+			var rowId = i + 1;
+			var colId = j + 1;
+			var cell = document.createElement("input");
+			cell.setAttribute("id", "b" + rowId + colId);
+			cell.setAttribute("type", "text");
+			row.appendChild(cell);
+			
+		}
+		tableBody.appendChild(row);
+	}
+	matrix2.appendChild(tableBody);
+}
+
+
+
+// **** FUNKTSIOON, MIS GENEREERIB VAHEPEALSE VASTUSE MAATRIKSI (see on praegu potentsiaalselt placeholder) ****
+
+function createMatrixAnswer() {
+	
+	var matrixAnswerContainer = document.getElementById("matrixAnswerContainer");
+	mAnswerWidth = 42 * m2y * 3;
+	mAnswerHeight = 28 * m1x;
+	
+	m1Width = 42 * m1y;
+	m2Width = 42 * m2y;
+	mAnswerPosition = m1Width + m2Width + 30;
+	
+	matrixAnswerContainer.style.width = mAnswerWidth + "px";
+	matrixAnswerContainer.style.height = mAnswerHeight + "px";
+	matrixAnswerContainer.style.left = mAnswerPosition + "px";
+	
+	var matrixAnswer = document.getElementById("matrixAnswer");
+	var tableBody = document.createElement("tbody");
+	
+	for(var i = 0; i < m1x; i++) {
+		var row = document.createElement("tr");
+		
+		for(var j = 0; j < m2y; j++) {
+			var rowId = i + 1;
+			var colId = j + 1;
+			var cell = document.createElement("input");
+			cell.setAttribute("id", "answer" + rowId + colId);
+			cell.setAttribute("class", "matrixAnswerInput");
+			cell.setAttribute("type", "text");
+			row.appendChild(cell);
+			
+		}
+		tableBody.appendChild(row);
+	}
+	matrixAnswer.appendChild(tableBody);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //EI TEA KAS ON KÕIGE OPTIMAALSEM VÄÄRTUSE SAAMISE VIIS
 function Calculate(){
